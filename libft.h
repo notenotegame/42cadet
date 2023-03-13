32907c4ec15b4a22182f89431c60b9dafced402e
notenotegame <notenotegame@hotmail.com>
2023-03-13 17:47:11 +0700

    added

--- libft.h
+++ libft.h
@@ -1,4 +1,3 @@
-
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -12,12 +11,13 @@ typedef struct	s_list
 }		t_list;
 
 void	ft_bzero(void *s, size_t n);
-
+void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 
+int	ft_atoi(const char *str);
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
 int	ft_isdigit(int c);
@@ -31,6 +31,7 @@ size_t	ft_strlen(char *str);
 
 
 char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *str);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
